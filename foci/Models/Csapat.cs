bd94eeddb4c8439a212b779fe7d1b5d5d1ff8300
majoros.peter <majoros.peter@BURG-1E03-10>
2024-09-19 12:51:31 +0200

    Add project files.

---
+++ foci/Models/Csapat.cs
@@ -0,0 +1,15 @@
+﻿namespace foci.Models
+{
+    public class Csapat
+    {
+        public string CsapatNev { get; set; }
+        public int Helyezes { get; set; }
+        public int JatszottMerkozesekSzama { get; set; }
+        public int LottGolok { get; set; }
+        public int KapottGolok { get; set; }
+        public int Nyert { get; set; }
+        public int Vesztett { get; set; }
+        public int Dontetlen { get; set; }
+        public int Pontszam { get; set; }
+    }
+}
